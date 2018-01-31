dcfd5d7e672271bb3566a4c813c39f4c3f104c6a
159753ss <dayup9843@163.com>
2018-01-31 13:34:59 +0800

    BannerDemo

--- app/src/main/java/com/wen_wen/bannerdemo/BannerAdapter.java
+++ app/src/main/java/com/wen_wen/bannerdemo/BannerAdapter.java
@@ -1,14 +1,25 @@
 package com.wen_wen.bannerdemo;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by WeLot on 2018/1/30.
  */
 
 public class BannerAdapter extends PagerAdapter {
+    private Context context;
+    private View.OnClickListener onBannerClickListener;
+
+    public BannerAdapter(Context context) {
+        this.context = context;
+    }
+
+    private int[] banners = new int[]{R.mipmap.a, R.mipmap.b, R.mipmap.c};
+
     @Override
     public int getCount() {
         return Integer.MAX_VALUE;
@@ -16,13 +27,43 @@ public class BannerAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return false;
+        return view == object;
     }
+
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        return super.instantiateItem(container, position);
+        position %= banners.length;//去余 position的取值范围是length-1
+        ImageView banner = new ImageView(context);
+        banner.setImageResource(banners[position]);
+        banner.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        banner.setTag(position);
+        banner.setOnClickListener(onClickListener);
+        container.addView(banner);
+        return banner;
+
     }
 
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (onBannerClickListener != null) {
+                onBannerClickListener.onClick(v);
+            }
+        }
+    };
 
+    public void setOnBannerClickListener(View.OnClickListener onBannerClickListener) {
+        this.onBannerClickListener = onBannerClickListener;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    public   int[]  getBanners(){
+
+        return   banners;
+    }
 }
--- app/src/main/java/com/wen_wen/bannerdemo/MainActivity.java
+++ app/src/main/java/com/wen_wen/bannerdemo/MainActivity.java
@@ -1,13 +1,119 @@
 package com.wen_wen.bannerdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ViewPager.OnPageChangeListener {
+
+    private static final long DELAY_TIME = 3000;
+    private ViewPager pager;
+    private LinearLayout point;
+    private BannerAdapter bannerAdapter;
+    private int currentItem = 0;
+    private Handler handler = new Handler();
+    private final Runnable mTicker = new Runnable() {
+        public void run() {
+            long now = SystemClock.uptimeMillis();
+            long next = now + (DELAY_TIME - now % DELAY_TIME);
+
+            handler.postAtTime(mTicker, next);//延迟5秒再次执行runnable,就跟计时器一样效果
+
+            currentItem++;
+            pager.setCurrentItem(currentItem);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pager = ((ViewPager) findViewById(R.id.banner_pager));
+        point = ((LinearLayout) findViewById(R.id.point_linear));
+        bannerAdapter = new BannerAdapter(this);
+        //缓存页数
+        //pager.setOffscreenPageLimit(2);
+        bannerAdapter.setOnBannerClickListener(this);
+        pager.addOnPageChangeListener(this);
+        pager.setAdapter(bannerAdapter);
+
+        initPoint();
+
+        //设置当前页
+
+        pager.setCurrentItem(currentItem);
+
+        handler.postDelayed(mTicker, DELAY_TIME);
+
+    }
+
+    private void initPoint() {
+        for (int i = 0; i < bannerAdapter.getBanners().length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(10, 10));
+            if (i == 0) {
+                imageView.setImageResource(R.drawable.point_select);
+            } else {
+                imageView.setImageResource(R.drawable.point_unselect);
+            }
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.
+                    LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            layoutParams.leftMargin = 10;
+            layoutParams.rightMargin = 10;
+            point.addView(imageView, layoutParams);
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        currentItem = position;
+        setPointSelect(currentItem);
+    }
+
+    private void setPointSelect(int selectItems) {
+
+        Log.d("111","selectItems:"+selectItems%bannerAdapter.getBanners().length);
+        for (int i = 0; i < bannerAdapter.getBanners().length; i++) {
+            ImageView imageView = (ImageView) point.getChildAt(i);
+            imageView.setBackgroundDrawable(null);//先把背景设置成无
+            if (i == (selectItems%bannerAdapter.getBanners().length)) {
+                imageView.setImageResource(R.drawable.point_select);
+            } else {
+                imageView.setImageResource(R.drawable.point_unselect);
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(mTicker);
     }
 }
